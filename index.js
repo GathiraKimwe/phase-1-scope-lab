8673bdf6e18d9e0712d10668f9df4ef43841f066
Edwin Gathira <gathirakimwe@gmail.com>
2022-08-22 19:21:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "asdffaf";
+
+function upperCaseCustomerName() {
+  return customerName = "BOB";
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "changing";
+  return leastFavoriteCustomer;
+}
